eb02b3ebbe8d4d4c65e7832a51185976d8de3033
Hugo Rodrigues <hugorod81@hotmail.com>
2023-01-10 00:40:40 -0300

    Business log created

--- src/BibleTextBot.Worker/ApplicationCore/Services/BotBibleTextService.cs
+++ src/BibleTextBot.Worker/ApplicationCore/Services/BotBibleTextService.cs
@@ -30,17 +30,25 @@ public class BotBibleTextService : IBotBibleTextService
     {
         var bibleTextFilter = new BibleTextFilter();
 
+        _logger.LogInformation("Biblical text capture process started at: {time}", DateTimeOffset.Now);
+
         foreach (var version in bibleTextFilter.VersoesFilter)
         {
             var bible = new Bible();
             var bibleVersion = new VersionBible(version.Abbreviation, version.Description);
 
+            _logger.LogInformation($"Bible version: Abbreviation {version.Abbreviation} | Description {version.Description}");
+
             foreach (var testament in version.TestamentFilters)
             {
                 var bibleTestament = new Testament(testament.Abbreviation, testament.Description);
 
+                _logger.LogInformation($"Bible Testament: Abbreviation {testament.Abbreviation} | Description {testament.Description}");
+
                 foreach (var book in testament.booksFilter)
                 {
+                    _logger.LogInformation("HTML Document capture process started at: {time}", DateTimeOffset.Now);
+
                     var url = $"{_settingBibleOnline.UrlBible}{bibleVersion.Abbreviation.Trim()}/{book.Abbreviation.Trim()}".ToLower();
                     var htmlDocument = await TextoUrlService.ObterHtmlDocumentAsync(url);
                     var chapterCount = htmlDocument
@@ -49,10 +57,14 @@ public class BotBibleTextService : IBotBibleTextService
                         .Descendants("li")
                         .Count();
 
+                    _logger.LogInformation("HTML Document capture process ended at: {time}", DateTimeOffset.Now);
+
                     var bibleChapters = new List<Chapter>();
 
                     for (int chapterNumber = 1; chapterNumber <= chapterCount; chapterNumber++)
                     {
+                        _logger.LogInformation("Chapter text capture process started at: {time}", DateTimeOffset.Now);
+
                         htmlDocument = await TextoUrlService.ObterHtmlDocumentAsync($"{url}/{chapterNumber}");
                         var verses = htmlDocument
                             .DocumentNode
@@ -65,6 +77,8 @@ public class BotBibleTextService : IBotBibleTextService
                             .ToList();
 
                         bibleChapters.Add(new Chapter(chapterNumber, verses));
+
+                        _logger.LogInformation("Chapter text capture process ended at: {time}", DateTimeOffset.Now);
                     }
 
                     bible.AddBook(new Book(book.Abbreviation, book.Description, bibleVersion, bibleTestament, bibleChapters));
@@ -73,10 +87,14 @@ public class BotBibleTextService : IBotBibleTextService
 
             RegisterBiblicalTextAsync(bible);
         }
+
+        _logger.LogInformation("Biblical text capture process ended at: {time}", DateTimeOffset.Now);
     }
 
     private void RegisterBiblicalTextAsync(Bible bible)
     {
+        _logger.LogInformation("Register Biblical Text started at: {time}", DateTimeOffset.Now);
+
         var insertBible = bible.BookItems.FirstOrDefault();
 
         if (insertBible != null)
@@ -86,5 +104,7 @@ public class BotBibleTextService : IBotBibleTextService
             .InsertOne(insertBible);
             //.InsertManyAsync(bible.BookItems);
         }
+
+        _logger.LogInformation("Register Biblical Text ended at: {time}", DateTimeOffset.Now);
     }
 }
