41ba16c4e9d3843ad9aee6f097645a1f93892bef
Hugo Rodrigues <hugorod81@hotmail.com>
2023-01-10 01:43:37 -0300

    Serilog configuration

--- src/BibleTextBot.Worker/Worker.cs
+++ src/BibleTextBot.Worker/Worker.cs
@@ -1,16 +1,36 @@
 ﻿namespace BibleTextBot.Worker;
 
 using ApplicationCore.Interfaces;
+using Serilog;
+using Serilog.Sinks.Elasticsearch;
 
 public class Worker : BackgroundService
 {
     private readonly ILogger<Worker> _logger;
     private readonly IBotBibleTextService _botTextoBiblicoService;
+    private readonly IConfiguration configuration;
 
-    public Worker(ILogger<Worker> logger, IBotBibleTextService botTextoBiblicoService)
+    public Worker(ILogger<Worker> logger,
+        IBotBibleTextService botTextoBiblicoService,
+        IConfiguration configuration)
     {
         _logger = logger;
         _botTextoBiblicoService = botTextoBiblicoService;
+        this.configuration = configuration;
+    }
+
+    public override Task StartAsync(CancellationToken cancellationToken)
+    {
+        var elasticUri = configuration
+            .GetSection("ElasticSettings")
+            .GetValue<string>("Uri");
+
+        Log.Logger = new LoggerConfiguration()
+            .Enrich.FromLogContext()
+            .WriteTo.Elasticsearch(new ElasticsearchSinkOptions(new Uri(elasticUri)) { AutoRegisterTemplate = true })
+            .CreateLogger();
+
+        return base.StartAsync(cancellationToken);
     }
 
     protected override async Task ExecuteAsync(CancellationToken stoppingToken)
